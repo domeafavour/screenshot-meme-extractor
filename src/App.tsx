43431e823ded5f441ad4628237c320002b3d0cea
zhanghaojie <1455123087@qq.com>
2024-02-29 11:40:22 +0800

    Add image clipping functionality and save button

--- src/App.tsx
+++ src/App.tsx
@@ -1,65 +1,73 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import './App.css';
 import { UploadButton } from './UploadButton';
+import { Area } from './typings';
 import {
   clearAndDrawImage,
+  downloadImage,
   drawBackdrops,
   drawClippedArea,
+  getClippedImage,
   getClippedRect,
   loadImageFile,
 } from './utils';
 
-async function renderImageFile(file: File, canvas: HTMLCanvasElement) {
-  const img = await loadImageFile(file);
-  const imageData = await clearAndDrawImage(img, canvas);
-  const clippedRect = getClippedRect(imageData);
-
-  if (clippedRect) {
-    const {
-      x: clippedX,
-      y: clippedY,
-      width: clippedWidth,
-      height: clippedHeight,
-    } = clippedRect;
-
-    drawClippedArea(canvas, {
-      x: clippedX,
-      y: clippedY,
-      width: clippedWidth,
-      height: clippedHeight,
-    });
-
-    drawBackdrops(canvas, {
-      x: clippedX,
-      y: clippedY,
-      width: clippedWidth,
-      height: clippedHeight,
-    });
-  }
-}
-
 function App() {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  const imageRef = useRef<HTMLImageElement | null>(null);
+  const [clippedArea, setClippedArea] = useState<Area | null>(null);
+
+  async function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const file = e.target.files?.[0];
+    if (file) {
+      const canvas = canvasRef.current!;
+      const img = await loadImageFile(file);
+      imageRef.current = img;
+      const clippedRect = getClippedRect(await clearAndDrawImage(img, canvas));
+      setClippedArea(clippedRect);
+
+      if (clippedRect) {
+        const {
+          x: clippedX,
+          y: clippedY,
+          width: clippedWidth,
+          height: clippedHeight,
+        } = clippedRect;
+
+        drawClippedArea(canvas, {
+          x: clippedX,
+          y: clippedY,
+          width: clippedWidth,
+          height: clippedHeight,
+        });
+
+        drawBackdrops(canvas, {
+          x: clippedX,
+          y: clippedY,
+          width: clippedWidth,
+          height: clippedHeight,
+        });
+      }
+    }
+    e.target.value = '';
+  }
+
   return (
     <div className="flex flex-col h-full">
       <div className="flex flex-row justify-center items-center flex-1">
         <canvas ref={canvasRef} />
       </div>
-      <div className="flex flex-row gap-4 p-2">
-        <UploadButton
-          onChange={(e) => {
-            const file = e.target.files?.[0];
-            if (file) {
-              renderImageFile(file, canvasRef.current!);
-            }
-            e.target.value = '';
-          }}
-        />
+      <div className="flex flex-row gap-4 p-2 font-mono">
+        <UploadButton onChange={handleChange} />
         <div className="w-1/2">
           <button
             type="button"
             className="text-white bg-blue-600 hover:bg-blue-500 px-2 py-1 rounded-md w-full disabled:bg-gray-300 disabled:cursor-not-allowed"
-            disabled
+            disabled={!clippedArea}
+            onClick={() => {
+              const dataURL = getClippedImage(imageRef.current!, clippedArea!);
+              downloadImage(dataURL, 'clipped.png');
+            }}
           >
             Save
           </button>
