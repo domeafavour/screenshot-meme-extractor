292f2ec2f6830fa09900a63cc084a8268f274b94
zhanghaojie <1455123087@qq.com>
2024-02-29 10:39:24 +0800

    Add UploadButton component and update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React, { useRef } from 'react';
 import './App.css';
+import { UploadButton } from './UploadButton';
 import { ImageRowInfo } from './typings';
 import {
-  getClippedInfo,
   clearAndDrawImageFile,
+  getClippedInfo,
   getImageRowInfos,
 } from './utils';
 
@@ -52,9 +53,8 @@ function App() {
       <div className="flex flex-row justify-center items-center flex-1">
         <canvas ref={canvasRef} />
       </div>
-      <div className="flex flex-row gap-2">
-        <input
-          type="file"
+      <div className="flex flex-row gap-4 p-2">
+        <UploadButton
           onChange={(e) => {
             const file = e.target.files?.[0];
             if (file) {
@@ -62,8 +62,16 @@ function App() {
             }
             e.target.value = '';
           }}
-          accept="image/*"
         />
+        <div className="w-1/2">
+          <button
+            type="button"
+            className="text-white bg-blue-600 hover:bg-blue-500 px-2 py-1 rounded-md w-full disabled:bg-gray-300 disabled:cursor-not-allowed"
+            disabled
+          >
+            Save
+          </button>
+        </div>
       </div>
     </div>
   );
---
+++ src/UploadButton.tsx
@@ -0,0 +1,31 @@
+import React, { useRef } from 'react';
+
+export type UploadButtonProps = {
+  onChange?: React.ChangeEventHandler<HTMLInputElement>;
+};
+
+export function UploadButton({ onChange }: UploadButtonProps) {
+  const uploadRef = useRef<HTMLInputElement | null>(null);
+  return (
+    <div className="cursor-pointer w-1/2 overflow-hidden relative">
+      <input
+        ref={uploadRef}
+        className="hidden"
+        type="file"
+        onChange={onChange}
+        accept="image/*"
+      />
+      <div className="absolute top-0 left-0 w-full">
+        <button
+          type="button"
+          className="text-white bg-green-600 hover:bg-green-500 px-2 py-1 rounded-md w-full"
+          onClick={() => {
+            uploadRef.current?.click();
+          }}
+        >
+          Upload
+        </button>
+      </div>
+    </div>
+  );
+}
